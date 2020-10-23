2fc610b031a9a162e6d7d0cb262fc28b0a97c1bd
Daniel Victor <daniel.silva@apinteractive.com.br>
2020-10-23 10:44:20 -0300

    Estudos sobre classe

---
+++ pooMensagem/Mensagem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pooMensagem
+{
+    class Mensagem
+    {
+        public string TextoMensagem;
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+        }
+
+    }
+}
---
+++ pooMensagem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pooMensagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1, msg2;
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Alô Mundo";
+            msg1.ExibirMensagem();
+
+            msg2 = new Mensagem();
+            msg2.TextoMensagem = "Segundo Objeto";
+            msg2.ExibirMensagem();
+            Console.ReadKey();
+        }
+    }
+}
